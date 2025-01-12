9c513b887a4e606d8b1a9a40c7eef2fa3bfd4274
abhijeet-hms-work <abhijeet.hms.work@gmail.com>
2025-01-12 11:34:01 +0530

    middle ware concept

---
+++ src/controllers/postController.js
@@ -0,0 +1,5 @@
+export async function createPost(req, res) {
+  // call ther service layer function
+
+  return res.json({ message: "post created successfully" });
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connect } from "mongoose";
 import connectDB from "./config/dbConfig.js";
+import { createPost } from "./controllers/postController.js";
 
 const PORT = 3000;
 
@@ -9,6 +10,21 @@ const app = express(); // create express app server instance
 app.get("/ping", (req, res) => {
   return res.json({ message: "ping" });
 });
+
+function m1(req, res, next) {
+  console.log("m1");
+  next();
+}
+function m2(req, res, next) {
+  console.log("m2");
+  next();
+}
+function m3(req, res, next) {
+  console.log("m3");
+  next();
+}
+
+app.post("/posts", [m1, m2, m3], createPost);
 
 app.listen(PORT, () => {
   console.log("server listening on: http://localhost:", PORT);
