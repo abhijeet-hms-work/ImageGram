7ef6af61f0d077ad64c800d280d8cb523ccb631c
abhijeet-hms-work <abhijeet.hms.work@gmail.com>
2025-01-15 00:11:36 +0530

    added gitignore

--- src/index.js
+++ src/index.js
@@ -7,24 +7,18 @@ const PORT = 3000;
 
 const app = express(); // create express app server instance
 
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
 app.get("/ping", (req, res) => {
+  // const name = req.params.name;
+  console.log(req.query);
+  console.log(req.body);
   return res.json({ message: "ping" });
 });
 
-function m1(req, res, next) {
-  console.log("m1");
-  next();
-}
-function m2(req, res, next) {
-  console.log("m2");
-  next();
-}
-function m3(req, res, next) {
-  console.log("m3");
-  next();
-}
-
-app.post("/posts", [m1, m2, m3], createPost);
+app.post("/posts", createPost);
 
 app.listen(PORT, () => {
   console.log("server listening on: http://localhost:", PORT);
